7a4c4d85f51802b2611db3926748d42f2db16e19
Xist <xist@xist.gg>
2023-08-21 19:43:17 -0700

    Add XcgsGameViewportClient
    Because this class MUST be configured in INI, make an explicit one here, so it's easier to know to configure this class rather than some random other one (like CommonGame)

---
+++ Source/XistCommonGameSample/UI/XcgsGameViewportClient.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2023 xist.gg
+
+#include "XcgsGameViewportClient.h"
+
+
+UXcgsGameViewportClient::UXcgsGameViewportClient(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
---
+++ Source/XistCommonGameSample/UI/XcgsGameViewportClient.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2023 xist.gg
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "CommonGameViewportClient.h"
+#include "XcgsGameViewportClient.generated.h"
+
+
+/**
+ * CommonGame Game Viewport Client
+ *
+ * Must be configured in DefaultEngine.ini:
+ * 
+ * [/Script/Engine.Engine]
+ * GameViewportClientClassName=/Script/XistCommonGameSample.XcgsGameViewportClient
+ */
+UCLASS()
+class UXcgsGameViewportClient : public UCommonGameViewportClient
+{
+	GENERATED_BODY()
+
+public:
+	UXcgsGameViewportClient(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+};
