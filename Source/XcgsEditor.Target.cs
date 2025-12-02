9905e65add79221a108f5cb59bb39fd90b7afc05
Xist <xist@xist.gg>
2025-12-01 16:48:37 -0800

    Update Target.cs for 5.7

--- Source/XcgsEditor.Target.cs
+++ Source/XcgsEditor.Target.cs
@@ -8,8 +8,8 @@ public class XcgsEditorTarget : TargetRules
 	public XcgsEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		ExtraModuleNames.AddRange( new string[] { "XistCommonGameSample" } );
 	}
