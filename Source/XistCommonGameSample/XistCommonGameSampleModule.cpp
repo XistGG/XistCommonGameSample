795b439d819917a9e0b7d112796023c1fe2dcf15
Xist <xist@xist.gg>
2025-12-01 17:23:23 -0800

    Rename module cpp

--- Source/XistCommonGameSample/XistCommonGameSampleModule.cpp
+++ Source/XistCommonGameSample/XistCommonGameSampleModule.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2023 xist.gg
 
-#include "XistCommonGameSample.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XistCommonGameSample, "XistCommonGameSample" );
